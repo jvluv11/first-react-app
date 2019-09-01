9f1a5cb69ef0d5c437c1c50f2b791121cae5c977
jvluv11 <jeanvladimir11@gmail.com>
2019-08-31 22:55:21 -0400

    Updates

---
+++ precache-manifest.ac9f544acff28d87dca8e6ed05df51d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df317154dbe4b59c7e7c73cdddd54623",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "81b80e0d0053d2a228d0",
+    "url": "/first-react-app/static/css/main.06318020.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/first-react-app/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "81b80e0d0053d2a228d0",
+    "url": "/first-react-app/static/js/main.7f377371.chunk.js"
+  },
+  {
+    "revision": "66d88166c4c2ea3ecd7f",
+    "url": "/first-react-app/static/js/runtime~main.c5839dd0.js"
+  }
+]);
